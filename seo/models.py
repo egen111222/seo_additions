0f11a58c3a9ed49a041eed22496b867ebfbe5c2c
Kirill <egen13@ukr.net>
2023-05-16 16:15:06 +0300

    add models of seo addition and seo tag

--- seo/models.py
+++ seo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class SeoAddition(models.Model):
+    element = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+    
+
+class SeoTag(models.Model):
+    tag = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.tag
+
+
