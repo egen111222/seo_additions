02b273e63820c37a6c6b5d36d482cd183452b253
Kirill <egen13@ukr.net>
2023-05-17 17:07:35 +0300

    seo additions and tags for files and projects

--- seo/models.py
+++ seo/models.py
@@ -1,17 +1,15 @@
 from django.db import models
 
-class SeoAddition(models.Model):
-    element = models.CharField(max_length=50)
-    description = models.CharField(max_length=250)
-
-    def __str__(self):
-        return self.name
-    
-
 class SeoTag(models.Model):
     tag = models.CharField(max_length=150)
 
     def __str__(self):
         return self.tag
 
+class SeoAddition(models.Model):
+    element = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    tags = models.ManyToManyField(SeoTag)
 
+    def __str__(self):
+        return self.element
