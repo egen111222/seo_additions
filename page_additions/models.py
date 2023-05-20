86d7ec636f6d0b093c637e7a3da9f65a5819c37e
Kirill <egen13@ukr.net>
2023-05-20 16:32:43 +0300

    with theme adding

--- page_additions/models.py
+++ page_additions/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Theme(models.Model):
+    name = models.CharField(max_length=250)
+    template = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
