e283d7f82e148dd1bc379ddfa61b08d15c0e2121
Kirill <egen13@ukr.net>
2023-05-20 16:43:48 +0300

    adding theme to admin panel

--- page_additions/admin.py
+++ page_additions/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from .extensions import (SeoKeyWordExtension,
                          ThemeExtension)
 from cms.extensions import PageExtensionAdmin
-
+from .models import Theme
 
 class PageExtensionAdmin(PageExtensionAdmin):
     pass
 
+admin.site.register(Theme)
 admin.site.register(SeoKeyWordExtension, PageExtensionAdmin)
 admin.site.register(ThemeExtension, PageExtensionAdmin)
